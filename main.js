0dbe1b14f99a3437e0ef25a4311c463f8cd49d81
ester-olsen <43723062+ester-olsen@users.noreply.github.com>
2019-04-17 20:19:11 +0200

    Initial commit.

---
+++ main.js
@@ -0,0 +1,82 @@
+(function() {
+    var timeline;
+
+    loadTimeline();
+
+    $('.url-input').change(function(event) {
+        var value = $(event.target).val();
+
+        $.get('https://cors-anywhere.herokuapp.com/' + value, function(response) {
+            var updatedDate = new Date($(response).find('div.header.module p.datetime').text());
+
+            var updatedDateString = updatedDate.getFullYear() + '-';
+            updatedDateString += updatedDate.getMonth() + 1 < 10 ? '0' : '';
+            updatedDateString += (updatedDate.getMonth() + 1) + '-';
+            updatedDateString += updatedDate.getDate() + 1 < 10 ? '0' : '';
+            updatedDateString += updatedDate.getDate() + 1;
+            
+            $('.date-input--updated').val(updatedDateString);
+
+            showEra(updatedDate, $('.era--updated'));
+        });
+    });
+
+    $('.date-input--published').change(function(event) {
+        var value = $(event.target).val();
+        var eraElement = $('.era--published');
+        
+        showEra(value, eraElement);
+    });
+
+    $('.date-input--updated').change(function(event) {
+        var value = $(event.target).val();
+        var eraElement = $('.era--updated');
+        
+        showEra(value, eraElement);
+    });
+
+    function showEra(date, eraElement) {
+        date = new Date(date);
+
+        var era = getEra(date);
+        var eraName = $(era).find('name').text();
+
+        $(eraElement).find('.era__era').text(eraName);
+    }
+
+    function getEra(date) {
+        date = new Date(date);
+
+        var earliestStartDate = new Date($(timeline).find('startDate').first().text());
+        var latestEndDate = new Date($(timeline).find('endDate').last().text());
+
+        if (date < earliestStartDate) {
+            return $(timeline).find('pre-timeline-era');
+        }
+        else if (date > latestEndDate) {
+            return $(timeline).find('post-timeline-era');
+        }
+
+        var eras = $(timeline).find('era');
+
+        for (let index = 0; index < eras.length; index++) {
+            var era = eras[index];
+            var startDate = new Date($(era).find('startDate').text());
+            var endDate = new Date($(era).find('endDate').text());
+
+            if ((date >= startDate) && (date <= endDate)) {
+                return era;
+            }
+        }
+
+        return null;
+    }
+
+    function loadTimeline() {
+        $.get('carmilla-timeline.xml', function(response) {
+            timeline = response;
+
+            $('body').fadeIn();
+        });
+    }
+})();
